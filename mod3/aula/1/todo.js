a498ba9bdc673d3485e1887029979ec7cd63626e
unknown <joao_pp_ferreira@saojose.sc.senai.br>
2019-06-17 08:44:26 -0300

    modulo 1 aula 2

---
+++ mod3/aula/1/todo.js
@@ -0,0 +1,8 @@
+var listElement = document.querySelector("#app ul");
+var InputElement = document.querySelector("#app input");
+var ButtonElement = document.querySelector("#app button");
+
+console.log(listElement);
+console.log(InputElement);
+console.log(ButtonElement);
+
